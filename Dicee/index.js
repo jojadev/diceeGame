bc44f645c2df37cb113b4ac4779dec5898c66189
Josh Jacobson <49420704+jojadev@users.noreply.github.com>
2022-05-15 20:08:40 -0400

    Add files via upload
    Added winner display and logic to site

--- Dicee/index.js
+++ Dicee/index.js
@@ -8,3 +8,17 @@ var randomnumber2 = Math.floor(Math.random() * 6 + 1);
 
 var dice2 = document.getElementById("dice2");
 dice2.setAttribute("src", `images/dice${randomnumber2}.png`);
+
+var results = document.getElementById("results-banner");
+
+if (randomnumber1 > randomnumber2) {
+  results.innerHTML = "Player 1 Wins!!";
+}
+
+if (randomnumber1 < randomnumber2) {
+  results.innerHTML = "Player 2 Wins!!";
+}
+
+if (randomnumber1 === randomnumber2) {
+  results.innerHTML = "Draw Game!!";
+}
